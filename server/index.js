fc21a53f9e4e54fe1575f5b8db92229a3a05d1f9
Miles Crighton <miles.crighton@gmail.com>
2020-02-26 14:17:52 +0000

    Corrected directory structure

---
+++ server/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const basicAuth = require('express-basic-auth');
+
+const auth = basicAuth({
+    users: {
+        admin: '123',
+        user: '456',
+    },
+});
+
+const PORT = 5000 || process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(require("body-parser").json());
+
+app.use(
+    express.static(path.join(__dirname, '../client/build'))
+).listen(
+    PORT, () => console.log(`Listening on ${PORT}`)
+);
+
+app.post('/authenticate', (req, res) => {
+    console.log(req.body)
+    if (req.body.auth.user === 'admin') {
+        res.send('admin');
+    } else if (req.body.auth.user === 'user') {
+        res.send('user');
+    }
+});
+
+app.get('/', (req, res) => {
+    //res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
